0a7c20d6053cda4c7644758e3b5224d74b0caf30
Jared O'Toole <jo39toole@yahoo.com>
2019-04-27 20:14:35 -0400

    updated

--- index.js
+++ index.js
@@ -3,26 +3,21 @@ AUTH: Jared O'Toole
 DATE: Saturday, February 23rd, 2019
 PROJ: Student Portfolio Webpage
 FILE: index.js
+
+(jquery required)
 */
+
 $(document).ready(function() {
     
-    // adds functionality to the block-1 header buttons
-    $(".block-1 .tab-nav .button").click(function() {
+    // make the tab-navs visible.
+    $(".tab-block .tab-nav").show();
+    
+    // add functionality to the tab-nav buttons
+    $(".tab-block .tab-nav .button").click(function() {
         var id = $(this).attr("class").split(/\s+/)[1];
-        $(".block-1 .tab").removeClass("active");
-        $(".block-1 #" + id + ".tab").addClass("active");
-        $(".block-1 .tab-nav .button").removeClass("selected");
+        $(".tab-block .tab").removeClass("active");
+        $(".tab-block #" + id + ".tab").addClass("active");
+        $(".tab-block .tab-nav .button").removeClass("selected");
         $(this).addClass("selected");
     });
-    
-    // adds functionality to the block-2 header buttons
-    $(".block-2 .tab-nav .button").click(function() {
-        var id = $(this).attr("class").split(/\s+/)[1];
-        $(".block-2 .tab").removeClass("active");
-        $(".block-2 #" + id + ".tab").addClass("active");
-        $(".block-2 .tab-nav .button").removeClass("selected");
-        $(this).addClass("selected");
-    });
-    
-
 });
